f1d3367bb4cbd1fc6154b1e4243f809a891b5d02
james <jamesafross@hotmail.co.uk>
2023-08-08 09:21:56 +0100

    path changes

--- Julia_Code/scripts/makeNextGeneration.jl
+++ Julia_Code/scripts/makeNextGeneration.jl
@@ -21,7 +21,8 @@ if size_pop < num_child
     @error "sizePop is larger than the number of child phenotypes!"
 end
 
-gen_phenotype = load("$DATADIR/Generation.jld", "GenPop")
+joinpath("$DATADIR","Generation.jl")
+gen_phenotype = load(joinpath("$DATADIR","Generation.jl"), "GenPop")
 if configuration["type_burst_analysis"] == "Threshold"
     file = matopen("$DATADIR/ThresholdStats.mat")
     phenotype_stats = read(file, "stats")
@@ -32,14 +33,14 @@ if configuration["type_burst_analysis"] == "Threshold"
     close(file)
 
 elseif configuration["type_burst_analysis"] == "HMM"
-    file = matopen("$DATADIR/HMMStats.mat")
+    file = matopen(joinpath("$DATADIR","HMMStats.mat"))
     phenotype_stats = read(file, "stats")
     close(file)
 
-    include("$DATADIR/HMMrealDataStats.jl")
+    include(joinpath("$DATADIR","HMMrealDataStats.jl"))
 end
 
-best_phenotype = load("$DATADIR/BestInGen.jld", "BestPhenotype")
+best_phenotype = load(joinpath("$DATADIR","BestInGen.jld"), "BestPhenotype")
 @test size(phenotype_stats) == (4, 2, size_pop)
 assign_stats(gen_phenotype, phenotype_stats)
 assign_fitness(gen_phenotype, realdata_stats)
@@ -51,23 +52,23 @@ best_fitness_params = best_in_gen.P
 bp = best_in_gen
 bp.Fitness = best_fitness
 best_phenotype = cat(best_phenotype, bp, dims=1)
-save("$DATADIR/BestInGen.jld", "BestPhenotype", best_phenotype)
+save(joinpath("$DATADIR","BestInGen.jld"), "BestPhenotype", best_phenotype)
 println("best phenotype in this generation is: \n $best_fitness_params. \n \n with total fitness: $best_fitness \n \n at index ", best_indx)
 
-open("$DATADIR/BestFitnessLog.txt", "w") do f #outputs best fitness phenotype to a .txt file
+open(joinpath("$DATADIR","BestFitnessLog.txt"), "w") do f #outputs best fitness phenotype to a .txt file
 end
 
 
 
 next_gen = make_next_gen(winners_tour, best_in_gen, size_pop, num_child, const_params)
-save("$DATADIR/LastGeneration.jld", "GenPop", gen_phenotype)
-save("$DATADIR/Generation.jld", "GenPop", next_gen)
+save(joinpath("$DATADIR","LastGeneration.jld"), "GenPop", gen_phenotype)
+save(joinpath("$DATADIR","Generation.jld"), "GenPop", next_gen)
 
 @testset "non-zeros phenotypes" begin
 end
 
 
-last_generation_data = JSON.parsefile("$PROGDIR/generation_results.json")
+last_generation_data = JSON.parsefile(joinpath("$PROGDIR","generation_results.json"))
 
 
 try
@@ -84,7 +85,7 @@ gen_data = Dict("Data" => [Dict("Generation" =>current_gen, "Fitness" => best_fi
 
 generation_data = Dict("Data" => append!(last_generation_data["Data"],gen_data["Data"]))
 
-open("$PROGDIR/generation_results.json","w") do f
+open(joinpath("$PROGDIR","generation_results.json"),"w") do f
     JSON.print(f, generation_data,4)
 end
 
