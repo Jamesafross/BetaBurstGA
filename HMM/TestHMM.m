7db2688d31b5230ac9a3bbecdf450453d1e42990
James Ross <pmxjr3@login001.int.augusta.nottingham.ac.uk>
2023-08-10 08:04:27 +0100

    HPC-side-changes

--- HMM/TestHMM.m
+++ HMM/TestHMM.m
@@ -1,12 +1,12 @@
-a=1
+
 clear all; close all;
 parpool('Threads')
 
-homedir=getuserdir();
-cd homedir + '/BetaBurstGA/HMM
-addpath(genpath(append('../HMM-MAR/')))
-addpath(genpath(append('../nutmeg/')))
-
+homedir=getuserdir()
+addpath(genpath(append('../HMM-MAR/')));
+addpath(genpath(append('../nutmeg/')));
+ls(homedir)
+cd(append(homedir,'/BetaBurstGA/HMM/'));
 fileName = '../config.json'; % filename in JSON extension
 str = fileread(fileName); % dedicated for reading files as text
 data = jsondecode(str); 
@@ -27,11 +27,12 @@ save('../datafiles/stats.mat', 'hmmstats')
 disp('Running HMM on all phenotypes in this generation')
 failcount = 0;
 T = 0;
-parfor i = 1:sizePop
+for i = 1:sizePop
 
     fprintf('Running HMM on phenotype %d \n',i)
+    stats = hmm_burst_detect_and_stats(squeeze(popcurrent(:,:,i)),numTrials,sampling_freq)
 
-    hmmstats(:,:,i) = supress_gather_output(i,popcurrent,numTrials,sampling_freq);
+    hmmstats(:,:,i) = stats
 end
 
 
@@ -41,11 +42,4 @@ stats = hmmstats;
 save('../datafiles/HMMStats.mat', 'stats')
 
 
-function stats = supress_gather_output(i,popcurrent,numTrials,sampling_freq)
-    try
-        [T,stats] = evalc('hmm_burst_detect_and_stats(squeeze(popcurrent(:,:,i)),numTrials,sampling_freq);');
-    catch
-        stats = zeros(4,2);
-        stats(:) = NaN;
-    end
-end
+    
